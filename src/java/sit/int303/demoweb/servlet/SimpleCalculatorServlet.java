84a238772f48ef3668cde71597832726a05256ff
Unknown <nuntuch13589@gmail.com>
2018-09-16 20:09:02 +0700

    ADD Test

---
+++ src/java/sit/int303/demoweb/servlet/SimpleCalculatorServlet.java
@@ -0,0 +1,94 @@
+package sit.int303.demoweb.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Nuntuch Thongyoo
+ */
+public class SimpleCalculatorServlet extends HttpServlet {
+ /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String xStr = request.getParameter("x");
+        String yStr = request.getParameter("y");
+        String operator = request.getParameter("oper");
+        
+        double xValue = Double.valueOf(xStr);
+        double yValue = Double.valueOf(yStr);
+        double result = xValue + yValue; 
+        
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet SimpleCalculatorServletO</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet SimpleCalculatorServletO at " + request.getContextPath() + "</h1>");
+            out.println("<h1 style='color:darkblue'>Servlet SimpleCalculatorO </h1><hr>");
+            out.print("&nbsp; &nbsp; &nbsp; &nbsp; x = "+ xValue +"<br>");
+            out.print("&nbsp; &nbsp; &nbsp; &nbsp; y = "+ yValue +"<hr>");
+            out.print("&nbsp; &nbsp; &nbsp; &nbsp; Result = "+ result +"<hr>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
+ 
